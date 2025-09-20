744b3610deef6401c1512ba67bdef54679c6fe22
Spidjo <spidjo@users.noreply.github.com>
2025-09-20 21:01:44 +0200

    verify

--- src/utils/email_service.py
+++ src/utils/email_service.py
@@ -71,7 +71,7 @@ def send_email_via_sendgrid(to_email, subject, body_text, body_html=None, attach
 
 def send_verification_email(to_email, username, token): 
     """Send account verification email using SendGrid"""
-    verify_url = f"{APP_URL}/verify_email?token={token}"
+    verify_url = f"{APP_URL}/verify_email?verify={token}"
     
     # Render HTML content from template
     template = templates_env.get_template("email_verification.html")
